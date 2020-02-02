c08d2721c5e00fcf05f447cd6939223f8a03f421
erinknowles0501 <colorandcontrast@gmail.com>
2020-02-02 00:17:06 -0400

    Some styling and getting auth setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://placekitten.com/400/400" class="rounded-circle w-100">
+        </div>
+        <div class="col-9 pt-5">
+            
+            <div><h1>Erin's CloneGram</h1></div>
+            
+            <div class="d-flex">
+                <div class="pr-5"><strong>Fake</strong> posts</div>
+                <div class="pr-5"><strong>Fake</strong> followers</div>
+                <div class="pr-5"><strong>Fake</strong> following</div>
             </div>
+            
         </div>
     </div>
+    
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://placekitten.com/400/200" class="w-100 p-3">
+        </div>
+        <div class="col-4">
+            <img src="https://placekitten.com/500/600" class="w-100 p-3">
+        </div>
+        <div class="col-4">
+            <img src="https://placekitten.com/300/500" class="w-100 p-3">
+        </div>
+    </div>
+    
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="https://bootswatch.com/4/litera/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -24,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Erin's CloneGram
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
