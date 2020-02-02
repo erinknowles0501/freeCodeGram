74148ae6fa993fb09ab3eb5b362aad8fbb06113e
erinknowles0501 <colorandcontrast@gmail.com>
2020-02-02 15:25:08 -0400

    Image resizing w/ Intervention Image

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -21,6 +22,11 @@ class PostsController extends Controller
         ]);
         
         $image_path = request('image')->store('uploads', 'public');
+        
+        $image = Image::make(public_path("storage/{$image_path}"))->fit(1200,1200, function($constraint) {
+            $constraint->upsize();
+        });
+        $image->save();
         
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
