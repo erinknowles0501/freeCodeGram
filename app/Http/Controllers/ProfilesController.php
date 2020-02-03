bffab83eb06d1d1c189e5b530f951861ebc01271
erinknowles0501 <colorandcontrast@gmail.com>
2020-02-02 21:27:05 -0400

    Upload/view profile pic, disallow editing or adding posts on not your own profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -14,30 +14,35 @@ class ProfilesController extends Controller
     }
     
     public function edit(User $user) {
+        $this->authorize('update', $user->profile);
         return view('profiles.edit', compact('user'));
     }
 
     public function update(User $user) {
+        $this->authorize('update', $user->profile);
+
+        
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
             'url' => 'url|required',
-            'image' => 'image|required',
+            'image' => 'image',
         ]);
         
-        $image_path = request('image')->store('uploads', 'public');
-        
-        $image = Image::make(public_path("storage/{$image_path}"))->fit(1200,1200, function($constraint) {
+        if (request('image')) {
+            $image_path = request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("storage/{$image_path}"))->fit(1200,1200, function($constraint) {
             $constraint->upsize();
-        });
-        $image->save();
+            });
+            $image->save();
+        }
         
-        $user->profile->update([
-            'title' => $data['title'],
-            'description' => $data['description'],
-            'url' => $data['url'],
-            'image' => $image_path,
-        ]);
+        
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $image_path]
+        ));
         
         return redirect('/profile/'.$user->id);
     }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,16 +4,22 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://placekitten.com/400/400" class="rounded-circle w-100">
+            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Add new post</a>
+                
+                @can('update', $user->profile)
+                    <a href="/p/create">Add new post</a>
+                @endcan
+                
             </div>
             
-            <a href="/profile/{{ $user->id }}/edit">Edit profile</a>
+            @can ('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit profile</a>
+            @endcan
             
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
