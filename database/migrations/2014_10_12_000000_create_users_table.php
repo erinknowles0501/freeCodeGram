ee41d12dd1a2da3ceb0cc046950baba1459d8ea9
erinknowles0501 <colorandcontrast@gmail.com>
2020-02-02 21:51:35 -0400

    Remove redundant image column from User table (it's handled in Profile)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
